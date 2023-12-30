0ec20487e44b29d613b170f06f6f089fa0b83661
lightstarts <1286518974@qq.com>
2023-12-31 00:16:24 +0800

    update

--- crawl.js
+++ crawl.js
@@ -10,46 +10,87 @@
 
 (function () {
     'use strict';
+    const questionContainer = document.querySelector('.clearfix');
+    function getJson() {
+        // 获取章节名称
+        const chapter = document.querySelector('.ceyan_name h3').innerText.trim().replace(/\n/g, '');
+        // 获取所有题目的容器
+        const questions = document.querySelectorAll('.TiMu');
 
-    // 获取所有题目的容器
-    const questions = document.querySelectorAll('.TiMu');
+        // 用于存储所有题目数据的数组
+        let data = [];
 
-    // 用于存储所有题目数据的数组
-    let data = [];
+        questions.forEach((question) => {
+            // 获取题目类型
+            const type = question.querySelector('.newZy_TItle').innerText.trim().replace(/\n/g, '').replace(/【|】/g, '');
 
-    questions.forEach((question) => {
-        // 获取题目类型
-        const type = question.querySelector('.newZy_TItle').innerText.trim().replace(/\n/g, '');
+            // 获取题干
+            const title = question.querySelector('.font-cxsecret').innerText.trim().replace(/\n/g, '');
 
-        // 获取题干
-        const title = question.querySelector('.font-cxsecret').innerText.trim().replace(/\n/g, '');
+            // 获取选项
+            let options = [];
+            const optionsList = question.querySelectorAll('.Zy_ulTop li');
+            if (type == "单选题" || type == "多选题") {
+                optionsList.forEach((option) => {
+                    const optionText = option.innerText.trim().replace(/\n/g, '');
+                    console.log(optionText);
+                    const optionKey = optionText.charAt(0);
+                    const optionValue = optionText.substring(2);
+                    options[optionKey] = optionValue;
+                    console.log(options);
+                    options.push({ [optionKey]: optionValue });
+                });
+            }
+            else if (type == "判断题") {
+                //判断题 固定 把选项设置成A. 对 B. 错
+                options.push({ "A": "对" });
+                options.push({ "B": "错" });
 
-        // 获取选项
-        let options = [];
-        const optionsList = question.querySelectorAll('.Zy_ulTop li');
-        optionsList.forEach((option) => {
-            options.push(option.innerText.trim().replace(/\n/g, ''));
+            }
+
+
+            // 获取正确答案,并将判断题的对错转换成A或B
+            const correctAnswer = question.querySelector('.correctAnswer .answerCon').innerText.trim().replace(/\n/g, '');
+            // 将题目数据添加到数组
+            data.push({
+                chapter,
+                type,
+                title,
+                options,
+                correctAnswer
+            });
         });
 
+        // 输出JSON格式的数据
+        console.log(JSON.stringify(data));
+        return data;
+    }
 
-        // 获取正确答案
-        const correctAnswer = question.querySelector('.correctAnswer .answerCon').innerText.trim().replace(/\n/g, '');
+    function exportJson() {
 
-        // 将题目数据添加到数组
-        data.push({
-            type,
-            title,
-            options,
-            correctAnswer
+        const button = document.createElement('button');
+        button.className = 'jb_btn_bg';
+        button.innerText = 'Export JSON';
+        button.addEventListener('click', () => {
+            const jsonData = JSON.stringify(getJson());
+            const blob = new Blob([jsonData], { type: 'application/json' });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'data.json';
+            a.click();
+            URL.revokeObjectURL(url);
         });
-    });
 
-    // 输出JSON格式的数据
-    console.log(JSON.stringify(data));
+        // Add the button to the document body
+        document.body.appendChild(button);
+    }
 
-    //下载JSON格式的数据
-    const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
-    const a = document.createElement('a');
-    a.download = 'data.json';
-    a.href = URL.createObjectURL(blob);
+
+    exportJson();
+
+
+
+
+
 })();
